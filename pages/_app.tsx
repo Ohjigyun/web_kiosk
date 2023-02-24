aaf87253ee3528a355bccc4503166db3c34e89a1
Ohjigyun <epraise12@naver.com>
2023-02-25 01:47:23 +0900

    styles: global font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Gamja_Flower } from 'next/font/google'
+
+const myFont = Gamja_Flower({
+    subsets: ['latin'],
+    weight: ['400']
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={myFont.className}>
+        <Component {...pageProps} />
+    </main>
+  )
 }
