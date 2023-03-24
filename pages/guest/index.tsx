384aa816943c23d4c950e5b90e3c477cd93a27cf
Ohjigyun <epraise12@naver.com>
2023-03-25 01:34:25 +0900

    style: guest page

--- pages/guest/index.tsx
+++ pages/guest/index.tsx
@@ -5,6 +5,7 @@ import { useAppSelector } from '../../app/hooks';
 import { signout, selectUser } from '../../app/slice/userSlice'
 import { persistor } from '../../app/store'
 import { auth, signOut } from '../api/firebase'
+import styles from '../../styles/GuestPage.module.css'
 
 
 export default function GuestPage(){
@@ -30,11 +31,38 @@ export default function GuestPage(){
     router.push(`/guest/${uid}/${tableNumber}`)
   }
 
+  const homeClickHandler = () => {
+    router.push('/')
+  }
+
+  const signoutClickHandler = () => {
+    signOut(auth)
+    persistor.purge()
+  }
+
   return (
-    <div>
-      <div>테이블 번호를 입력해 주세요.</div>
-      <input value={tableNumber} type="number" onChange={tableNumberChangeHandler}></input>
-      <button onClick={moveGuestPageClickHandler}>입장</button>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.headerRight}>
+          <div className={styles.homeAndSignout}>
+            <div className={styles.home} onClick={homeClickHandler}>홈으로</div>
+            <div className={styles.signout} onClick={signoutClickHandler}>로그아웃</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.body}>
+        <div className={styles.centerBox}>
+          <div className={styles.instruction}>
+            테이블 번호를 입력해 주세요.
+          </div>
+          <div className={styles.tableNumber}>
+            <input className={styles.inputTableNumber} value={tableNumber} type="number" onChange={tableNumberChangeHandler}></input>
+          </div>
+          <div>
+            <button className={styles.enterButton}onClick={moveGuestPageClickHandler}>입장</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
